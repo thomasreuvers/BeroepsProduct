c791dd0830b4a0e8a09692089e17fc356b90179c
Joost <jmp.ponsioen@student.han.nl>
2021-10-13 11:59:35 +0200

    Changed constructor in GameLevel

--- src/main/java/com/game/components/scenes/GameLevel.java
+++ src/main/java/com/game/components/scenes/GameLevel.java
@@ -1,11 +1,18 @@
 package com.game.components.scenes;
 
+import com.game.Game;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 
 public class GameLevel extends DynamicScene {
+  private Game game;
+
+  public GameLevel(Game game){
+    this.game = game;
+  }
+
   @Override
   public void setupScene() {
-
+      setBackgroundImage("backgrounds/background.png");
   }
 
   @Override
