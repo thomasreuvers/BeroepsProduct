76cbdf6660285dbaea773c80a4669f720a3d5e9f
Thomas <thomasreuvers@upcmail.nl>
2021-10-18 15:18:19 +0200

    Add PlatformSpawner

--- src/main/java/com/game/components/scenes/GameLevel.java
+++ src/main/java/com/game/components/scenes/GameLevel.java
@@ -1,15 +1,14 @@
 package com.game.components.scenes;
 
 import com.game.Game;
-import com.game.components.entities.platforms.GrassPlatform;
+import com.game.components.entities.PlayerIdle;
+import com.game.components.spawners.PlatformSpawner;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.EntitySpawnerContainer;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class GameLevel extends DynamicScene {
+public class GameLevel extends DynamicScene implements EntitySpawnerContainer {
 
   private Game game;
 
@@ -25,19 +24,12 @@ public class GameLevel extends DynamicScene {
   @Override
   public void setupEntities() {
 
-    // TODO extract code and make spawner class
-    Timer timer = new Timer();
-    timer.schedule(new TimerTask() {
-      @Override
-      public void run() {
-        var rand = new Random();
-
-        var size = new Size(100, 300);
-        addEntity(new GrassPlatform(rand.nextInt((int) (800 - size.width())), size, 2.0));
-      }
-    }, 0, 1000);
-
+    addEntity(new PlayerIdle(new Coordinate2D(400,100), new Size(300, 300)));
   }
 
+  @Override
+  public void setupEntitySpawners() {
+      addEntitySpawner(new PlatformSpawner(new Size(100, 300)));
+  }
 
 }
