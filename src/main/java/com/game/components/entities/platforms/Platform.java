811eebe28c8c7122ed88c675f59ebdae80db9f91
Thomas <thomasreuvers@upcmail.nl>
2021-10-19 17:27:08 +0200

    Remove unused code and add auto platform cleaning

--- src/main/java/com/game/components/entities/platforms/Platform.java
+++ src/main/java/com/game/components/entities/platforms/Platform.java
@@ -9,21 +9,14 @@ import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
 
 public class Platform extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, Collider {
-    private final Coordinate2D initialLocation;
 
     public Platform(String resource, Coordinate2D initialLocation, Size size, double speed) {
         super(resource, initialLocation, size);
         setMotion(speed, Direction.DOWN);
-
-        this.initialLocation = initialLocation;
     }
 
     @Override
     public void notifyBoundaryCrossing(SceneBorder border) {
-
-    }
-
-    public Coordinate2D getInitialLocation() {
-        return initialLocation;
+        remove();
     }
 }
