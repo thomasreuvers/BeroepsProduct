a18d97c612e463e9d08afa98087f426043ab172d
Thomas <thomasreuvers@upcmail.nl>
2021-10-19 14:59:44 +0200

    Fix jumping code

--- src/main/java/com/game/components/entities/Player.java
+++ src/main/java/com/game/components/entities/Player.java
@@ -6,35 +6,43 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.Newtonian;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
 
+import java.util.Collection;
 import java.util.Set;
 
 public class Player extends DynamicSpriteEntity implements KeyListener, Collider, Collided, Newtonian {
 
+    private boolean isFalling = true;
+
     public Player(Coordinate2D initialLocation, Size size, ScoreText scoreText) {
         super("sprites/player/player.png", initialLocation, size);
 
-//        setAutoCycle(40, 0);
         setGravityConstant(0);
         setFrictionConstant(0);
     }
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
-        if (pressedKeys.contains(KeyCode.SPACE)){
-            this.setAnchorLocationY(this.getAnchorLocation().getY() - 100.0);
-            System.out.println("Jump");
-        }else if (pressedKeys.contains(KeyCode.ENTER)) {
+        if (pressedKeys.contains(KeyCode.SPACE) && !isFalling) {
+            setMotion(getSpeed() * 1.5, Direction.UP);
+            setGravityConstant(0.3);
+            setFrictionConstant(0.04);
+            isFalling = true;
+        }else if (pressedKeys.contains(KeyCode.SPACE) && pressedKeys.contains(KeyCode.A) && !isFalling){
+
+        }
+        else if (pressedKeys.contains(KeyCode.ENTER)) {
             setGravityConstant(0.3);
             setFrictionConstant(0.04);
         }else if (pressedKeys.contains(KeyCode.A)){
-            setAnchorLocationX(this.getAnchorLocation().getX() - 10);
+            setAnchorLocationX(this.getAnchorLocation().getX() - 1 * getSpeed());
         }else if (pressedKeys.contains(KeyCode.D)){
-            setAnchorLocationX(this.getAnchorLocation().getX() + 10);
+            setAnchorLocationX(this.getAnchorLocation().getX() + 1 * getSpeed());
         }
     }
 
@@ -43,6 +51,7 @@ public class Player extends DynamicSpriteEntity implements KeyListener, Collider
         if (collidingObject instanceof Platform && this.getAnchorLocation().getY() + this.getHeight() > collidingObject.getBoundingBox().getMinY())
         {
             this.setAnchorLocationY(((Platform) collidingObject).getAnchorLocation().getY() - this.getHeight());
+            this.isFalling = false;
         }
     }
 }
